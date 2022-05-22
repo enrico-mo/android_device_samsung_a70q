b67999df9e33a40812c2eb4b264021f4074d8480
enrico-mo <enrico.caporali.mo@gmail.com>
2022-05-22 02:14:46 +0200

    Rebase from 20200629 Commit (SHA 64afb837486e030854a02f0dfadcfac701f36211) - now working

--- lineage_a70q.mk
+++ lineage_a70q.mk
@@ -1,21 +1,10 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64_ab.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
@@ -33,13 +22,11 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A705
 PRODUCT_MANUFACTURER := samsung
 
-# Use the latest approved GMS identifiers
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+BUILD_FINGERPRINT := "samsung/a70qxx/a70q:9/PPR1.180610.011/A705FNXXU5ASL4:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=a70q \
-    TARGET_BOOTLOADER_BOARD_NAME=sm6150 \
-    PRODUCT_NAME=a70qxx \
-    PRIVATE_BUILD_DESC="a70qxx-user 10 QP1A.190711.020 A705FNXXU5CTK4 release-keys" \
+    PRIVATE_BUILD_DESC="a70qxx-user 9 PPR1.180610.011 A705FNXXU5ASL4 release-keys" \
+    PRODUCT_NAME="a70q" \
+    TARGET_DEVICE="a70q"
 
-BUILD_FINGERPRINT := "samsung/a70qeea/a70q:10/QP1A.190711.020/A705FNXXU5CTK4:user/release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
